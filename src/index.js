e8ebd5fa75dd7555e1d0fb51a2ff8c82f9e80a94
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-05-25 17:24:28 +0100

    simple query, starting simple mutation

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("apollo-server");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
+};
+
+const server = new ApolloServer({
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
